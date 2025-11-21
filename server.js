e129a39888cb9e0a46923cddb415a5756e0217b3
nipuni-Gurusinghe <amashagurusinghe2003@gmail.com>
2025-11-21 11:42:33 +0530

    Initial commit on develop branch

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Nephromind Backend Working!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
